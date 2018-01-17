afd9eaa150e1a2f4a2c52a0f36e3b6125ff913b8
hardwareadictos <hardwareadictos@gmail.com>
2018-01-17 22:28:50 +0100

    oneplus2: ALLOW_MISSING_DEPENDENCIES

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,9 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
 # Cpusets
 ENABLE_SCHED_BOOST := true
 
+# Dependencies
+ALLOW_MISSING_DEPENDENCIES=true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 
